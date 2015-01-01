a1af5c01f8d1a448c95374f563c78c568d3ff01a
Laszlo Balazs-Csiki <lbalazscs@users.noreply.github.com>
2015-01-01 17:13:38 +0100

    comments

--- src/main/java/pixelitor/menus/edit/RedoMenuItem.java
+++ src/main/java/pixelitor/menus/edit/RedoMenuItem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2014 Laszlo Balazs-Csiki
+ * Copyright (c) 2015 Laszlo Balazs-Csiki
  *
  * This file is part of Pixelitor. Pixelitor is free software: you
  * can redistribute it and/or modify it under the terms of the GNU
@@ -8,11 +8,11 @@
  *
  * Pixelitor is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Pixelitor.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Pixelitor. If not, see <http://www.gnu.org/licenses/>.
  */
 package pixelitor.menus.edit;
 
@@ -23,7 +23,7 @@ import javax.swing.event.UndoableEditEvent;
 import javax.swing.event.UndoableEditListener;
 
 /**
- *
+ * The "Edit/Redo" menu item
  */
 public class RedoMenuItem extends JMenuItem implements UndoableEditListener {
     public RedoMenuItem(Action a) {
