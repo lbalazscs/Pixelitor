8a6f213d6a7220d889044023395e721c76489a90
Laszlo Balazs-Csiki <lbalazscs@users.noreply.github.com>
2015-06-25 00:10:33 +0200

    upgrade version

--- src/main/java/pixelitor/Build.java
+++ src/main/java/pixelitor/Build.java
@@ -73,7 +73,7 @@ public enum Build {
 
     public static Build CURRENT = FINAL;
 
-    public static final String VERSION_NUMBER = "3.1.2";
+    public static final String VERSION_NUMBER = "3.1.3";
 
     private static String fixTitle = null;
 
