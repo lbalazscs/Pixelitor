f116b7b8ad55c109ad4933912214b9f8a422be97
Laszlo Balazs-Csiki <lbalazscs@users.noreply.github.com>
2015-07-09 19:35:02 +0200

    fix unit test

--- src/test/java/pixelitor/layers/LayerBlendingModesTest.java
+++ src/test/java/pixelitor/layers/LayerBlendingModesTest.java
@@ -6,10 +6,9 @@ import pixelitor.Composition;
 import pixelitor.TestHelper;
 import pixelitor.history.AddToHistory;
 import pixelitor.utils.ImageUtils;
+import pixelitor.utils.Tests3x3;
 
 import java.awt.image.BufferedImage;
-
-import static pixelitor.utils.Tests3x3.rgbToPackedInt;
 
 public class LayerBlendingModesTest {
     private Composition comp;
@@ -31,25 +30,11 @@ public class LayerBlendingModesTest {
     @Test
     public void testNormal() {
         BufferedImage result = comp.calculateCompositeImage();
-        assert ImageUtils.compareSmallImages(result, getExpectedImageSingleLayer());
+        assert ImageUtils.compareSmallImages(result, Tests3x3.getStandardImage2());
     }
 
     @Test
     public void testMultiply() {
         upperLayer.setBlendingMode(BlendingMode.MULTIPLY, false, AddToHistory.YES, true);
-    }
-
-    BufferedImage getExpectedImageSingleLayer() {
-        BufferedImage img = ImageUtils.createCompatibleImage(3, 3);
-        img.setRGB(0, 0, rgbToPackedInt(255, 255, 0, 0));
-        img.setRGB(0, 1, rgbToPackedInt(255, 255, 255, 255));
-        img.setRGB(0, 2, rgbToPackedInt(255, 255, 255, 0));
-        img.setRGB(1, 0, rgbToPackedInt(255, 0, 255, 0));
-        img.setRGB(1, 1, rgbToPackedInt(255, 128, 128, 128));
-        img.setRGB(1, 2, rgbToPackedInt(255, 255, 0, 255));
-        img.setRGB(2, 0, rgbToPackedInt(255, 0, 0, 255));
-        img.setRGB(2, 1, rgbToPackedInt(255, 0, 0, 0));
-        img.setRGB(2, 2, rgbToPackedInt(255, 0, 255, 255));
-        return img;
     }
 }
