e2aedd49c5d3e0327581ee721845adfd15e288ec
Laszlo Balazs-Csiki <lbalazscs@users.noreply.github.com>
2018-05-30 23:13:54 +0200

    progress bar for the Canny edge detector

--- src/main/java/pixelitor/utils/DebugProgressTracker.java
+++ src/main/java/pixelitor/utils/DebugProgressTracker.java
@@ -22,41 +22,58 @@ import java.util.List;
 
 /**
  * A progress tracker that is used only for development
+ * to check that the work units were correctly estimated.
+ *
+ * It can be a decorator for another {@link ProgressTracker}
+ * to get both the statistics and the visual feedback.
  */
 public class DebugProgressTracker implements ProgressTracker {
     private final long startTimeMillis;
-    private final List<CallInfo> logs;
+    private final List<CallInfo> callInfos;
     private final String name;
     private final int expectedTotalUnits;
+    private final ProgressTracker delegateTracker;
     private long lastTime;
     private int calledUnits;
 
-    public DebugProgressTracker(String name, int expectedTotalUnits) {
+    public DebugProgressTracker(String name, int expectedTotalUnits, ProgressTracker delegateTracker) {
         this.name = name;
         this.expectedTotalUnits = expectedTotalUnits;
+        this.delegateTracker = delegateTracker;
         startTimeMillis = System.currentTimeMillis();
         lastTime = 0;
         calledUnits = 0;
-        logs = new ArrayList<>();
-        log("created");
+        callInfos = new ArrayList<>();
+        log("created " + name + ", expectedTotalUnits = " + expectedTotalUnits);
     }
 
     @Override
     public void unitDone() {
+        if (delegateTracker != null) {
+            delegateTracker.unitDone();
+        }
+
         calledUnits++;
         log("unitDone");
     }
 
     @Override
     public void unitsDone(int units) {
+        if (delegateTracker != null) {
+            delegateTracker.unitsDone(units);
+        }
+
         calledUnits += units;
         log("addUnits " + units);
     }
 
     @Override
     public void finish() {
+        if (delegateTracker != null) {
+            delegateTracker.finish();
+        }
+
         log("finish");
-        long totalDuration = System.currentTimeMillis() - startTimeMillis;
 
         System.out.print("Progress for " + name + ", received units = " + calledUnits);
         if (calledUnits == expectedTotalUnits) {
@@ -64,35 +81,46 @@ public class DebugProgressTracker implements ProgressTracker {
         } else {
             System.out.println(", NOK, expectedTotalUnits = " + expectedTotalUnits);
         }
-        logs.stream()
+
+        printStatisticsAboutEachCall();
+        System.out.println();
+    }
+
+    private void printStatisticsAboutEachCall() {
+        long totalDuration = System.currentTimeMillis() - startTimeMillis;
+
+        callInfos.stream()
                 .map(callInfo -> callInfo.asString(totalDuration))
                 .map(s -> s.replace("pixelitor.", ""))
                 .map(s -> s.replace("filters.", ""))
                 .map(s -> s.replace("jhlabsproxies.", ""))
                 .map(s -> s.replace("utils.", ""))
                 .forEach(System.out::println);
-        System.out.println();
     }
 
-    private void log(String what) {
+    private void log(String method) {
         long time = System.currentTimeMillis() - startTimeMillis;
         StackTraceElement ste = new Throwable().getStackTrace()[2];
 
-        logs.add(new CallInfo(what, time, lastTime, ste));
+        callInfos.add(new CallInfo(method, time, lastTime, ste, expectedTotalUnits));
 
         lastTime = time;
     }
 
-
+    /**
+     * Information about one call to a {@link DebugProgressTracker} method.
+     */
     private static class CallInfo {
         private final StackTraceElement ste;
-        private final String what;
+        private final String method;
         private final long time;
+        private final int totalUnits;
         private final long duration;
 
-        public CallInfo(String what, long time, long lastTime, StackTraceElement ste) {
-            this.what = what;
+        public CallInfo(String method, long time, long lastTime, StackTraceElement ste, int totalUnits) {
+            this.method = method;
             this.time = time;
+            this.totalUnits = totalUnits;
             this.duration = time - lastTime;
             this.ste = ste;
         }
@@ -103,9 +131,11 @@ public class DebugProgressTracker implements ProgressTracker {
 
             double durationPercentage = (duration * 100.0) / totalDuration;
 
-            String whatWithPercent = String.format("%s (%.1f%%)", what, durationPercentage);
+            String whatWithPercent = String
+                    .format("%s (%.1f%%=>%.2fu)", method, durationPercentage, (durationPercentage / 100.0) * totalUnits);
 
-            return String.format("%.2f:%.2f: %-21s at %s.%s(%s:%d)", timeSeconds, durationSeconds, whatWithPercent,
+            return String
+                    .format("%.2fs (dur=%.2fs): %-21s at %s.%s(%s:%d)", timeSeconds, durationSeconds, whatWithPercent,
                     ste.getClassName(), ste.getMethodName(), ste.getFileName(), ste.getLineNumber());
         }
     }
