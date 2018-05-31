5ae3af7b91e53cbdfa5780a42d58e660f5caff46
Laszlo Balazs-Csiki <lbalazscs@users.noreply.github.com>
2018-05-31 20:15:20 +0200

    MotionBlurFilter optimizations

--- src/main/java/com/jhlabs/image/MotionBlurFilter.java
+++ src/main/java/com/jhlabs/image/MotionBlurFilter.java
@@ -16,9 +16,13 @@ limitations under the License.
 
 package com.jhlabs.image;
 
-import java.awt.geom.AffineTransform;
+import net.jafama.DoubleWrapper;
+import net.jafama.FastMath;
+import pixelitor.ThreadPool;
+
 import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
+import java.util.concurrent.Future;
 
 /**
  * A filter which produces motion blur the slow, but higher-quality way.
@@ -263,89 +267,147 @@ public class MotionBlurFilter extends AbstractBufferedImageOp implements MotionB
         int cx = (int) (width * centreX);
         int cy = (int) (height * centreY);
 
-        int index = 0;
+//        int index = 0;
 
         float imageRadius = (float) Math.sqrt(cx * cx + cy * cy);
         float translateX = (float) (distance * Math.cos(angle));
         float translateY = (float) (distance * -Math.sin(angle));
         float maxDistance = distance + Math.abs(rotation * imageRadius) + zoom * imageRadius;
         int repetitions = (int) maxDistance;
-        AffineTransform t = new AffineTransform();
-        Point2D.Float p = new Point2D.Float();
 
         if (premultiplyAlpha) {
             ImageMath.premultiply(inPixels, 0, inPixels.length);
         }
+
+        Future<?>[] futures = new Future[height];
         for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int a = 0, r = 0, g = 0, b = 0;
-                int count = 0;
-                for (int i = 0; i < repetitions; i++) {
-                    int newX = x, newY = y;
-                    float f = (float) i / repetitions;
-
-                    p.x = x;
-                    p.y = y;
-                    t.setToIdentity();
-                    t.translate(cx + f * translateX, cy + f * translateY);
-                    float s = 1 - zoom * f;
-                    t.scale(s, s);
-                    if (rotation != 0) {
-                        t.rotate(-rotation * f);
-                    }
-                    t.translate(-cx, -cy);
-                    t.transform(p, p);
-                    newX = (int) p.x;
-                    newY = (int) p.y;
-
-                    if (newX < 0 || newX >= width) {
-                        if (wrapEdges) {
-                            newX = ImageMath.mod(newX, width);
-                        } else {
-                            break;
-                        }
-                    }
-                    if (newY < 0 || newY >= height) {
-                        if (wrapEdges) {
-                            newY = ImageMath.mod(newY, height);
-                        } else {
-                            break;
-                        }
-                    }
-
-                    count++;
-                    int rgb = inPixels[newY * width + newX];
-                    a += (rgb >> 24) & 0xff;
-                    r += (rgb >> 16) & 0xff;
-                    g += (rgb >> 8) & 0xff;
-                    b += rgb & 0xff;
-                }
-                if (count == 0) {
-                    outPixels[index] = inPixels[index];
-                } else {
-                    a = PixelUtils.clamp(a / count);
-                    r = PixelUtils.clamp(r / count);
-                    g = PixelUtils.clamp(g / count);
-                    b = PixelUtils.clamp(b / count);
-                    outPixels[index] = (a << 24) | (r << 16) | (g << 8) | b;
-				}
-				index++;
-			}
-            pt.unitDone();
-		}
-        if ( premultiplyAlpha ) {
+            int finalY = y;
+            Runnable lineTask = () -> calcLine(width, height, inPixels, outPixels, cx, cy, translateX, translateY, repetitions, finalY);
+            futures[y] = ThreadPool.submit(lineTask);
+        }
+        ThreadPool.waitForFutures(futures, pt);
+        if (premultiplyAlpha) {
             ImageMath.unpremultiply(outPixels, 0, inPixels.length);
         }
 
-        setRGB( dst, 0, 0, width, height, outPixels );
+        setRGB(dst, 0, 0, width, height, outPixels);
 
         finishProgressTracker();
 
         return dst;
     }
 
-	public String toString() {
-		return "Blur/Motion Blur...";
-	}
+    private void calcLine(int width, int height, int[] inPixels, int[] outPixels, int cx, int cy, float translateX, float translateY, int repetitions, int y) {
+        int index = y * width;
+        FastTransform t = new FastTransform();
+        Point2D.Float p = new Point2D.Float();
+
+        for (int x = 0; x < width; x++) {
+            int a = 0, r = 0, g = 0, b = 0;
+            int count = 0;
+            for (int i = 0; i < repetitions; i++) {
+                int newX = x, newY = y;
+
+                if (i != 0) {
+                    float f = (float) i / repetitions;
+                    p.x = x;
+                    p.y = y;
+                    t.setToIdentity();
+                    t.translateAfterIdentity(cx + f * translateX, cy + f * translateY);
+                    float s = 1 - zoom * f;
+                    t.scaleAfterTranslate(s, s);
+                    if (rotation != 0) {
+                        t.rotate(-rotation * f);
+                    }
+                    t.translateAfterRotate(-cx, -cy);
+                    t.transform(p, p);
+                    newX = (int) p.x;
+                    newY = (int) p.y;
+                }
+                if (newX < 0 || newX >= width) {
+                    if (wrapEdges) {
+                        newX = ImageMath.mod(newX, width);
+                    } else {
+                        break;
+                    }
+                }
+                if (newY < 0 || newY >= height) {
+                    if (wrapEdges) {
+                        newY = ImageMath.mod(newY, height);
+                    } else {
+                        break;
+                    }
+                }
+
+                count++;
+                int rgb = inPixels[newY * width + newX];
+                a += (rgb >> 24) & 0xff;
+                r += (rgb >> 16) & 0xff;
+                g += (rgb >> 8) & 0xff;
+                b += rgb & 0xff;
+            }
+            if (count == 0) {
+                outPixels[index] = inPixels[index];
+            } else {
+                a = PixelUtils.clamp(a / count);
+                r = PixelUtils.clamp(r / count);
+                g = PixelUtils.clamp(g / count);
+                b = PixelUtils.clamp(b / count);
+                outPixels[index] = (a << 24) | (r << 16) | (g << 8) | b;
+            }
+            index++;
+        }
+    }
+
+    public String toString() {
+        return "Blur/Motion Blur...";
+    }
+
+    // an affine transform optimized for this specific filter
+    static class FastTransform {
+        private double m00, m10, m01, m11, m02, m12;
+        private final DoubleWrapper doubleWrapper = new DoubleWrapper();
+
+        public void setToIdentity() {
+            m00 = m11 = 1.0;
+            m10 = m01 = m02 = m12 = 0.0;
+        }
+
+        public void translateAfterIdentity(double tx, double ty) {
+            m02 = tx;
+            m12 = ty;
+        }
+
+        public void scaleAfterTranslate(double sx, double sy) {
+            m00 = sx;
+            m11 = sy;
+        }
+
+        public void rotate(double theta) {
+            double sin = FastMath.sinAndCos(theta, doubleWrapper);
+            double cos = doubleWrapper.value;
+            double m0 = m00;
+            double m1 = m01;
+            m00 = cos * m0 + sin * m1;
+            m01 = -sin * m0 + cos * m1;
+            m0 = m10;
+            m1 = m11;
+            m10 = cos * m0 + sin * m1;
+            m11 = -sin * m0 + cos * m1;
+        }
+
+        public void translateAfterRotate(double tx, double ty) {
+            m02 = tx * m00 + ty * m01 + m02;
+            m12 = tx * m10 + ty * m11 + m12;
+        }
+
+        public Point2D transform(Point2D ptSrc, Point2D ptDst) {
+            double x = ptSrc.getX();
+            double y = ptSrc.getY();
+            ptDst.setLocation(x * m00 + y * m01 + m02,
+                    x * m10 + y * m11 + m12);
+            return ptDst;
+        }
+    }
 }
 
