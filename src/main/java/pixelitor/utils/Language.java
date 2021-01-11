13fd679a7eaf6569a790df5ab063cf7d5d9ea09f
Laszlo Balazs-Csiki <lbalazscs@users.noreply.github.com>
2021-01-11 19:09:45 +0100

    added Dutch to the languages in Preferences

--- src/main/java/pixelitor/utils/Language.java
+++ src/main/java/pixelitor/utils/Language.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Laszlo Balazs-Csiki and Contributors
+ * Copyright 2021 Laszlo Balazs-Csiki and Contributors
  *
  * This file is part of Pixelitor. Pixelitor is free software: you
  * can redistribute it and/or modify it under the terms of the GNU
@@ -24,10 +24,11 @@ import java.util.Locale;
  * The languages supported by Pixelitor
  */
 public enum Language {
+    DUTCH("Dutch", "nl") {},
     ENGLISH("English", "en") {},
+    FRENCH("French", "fr") {},
     GERMAN("German", "de") {},
-    SPANISH("Spanish", "es") {},
-    FRENCH("French", "fr") {};
+    SPANISH("Spanish", "es") {};
 
     private final String guiName;
     private final String code;
