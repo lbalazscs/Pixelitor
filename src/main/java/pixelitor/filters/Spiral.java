e5aded25802f12ff41ebb753da92447bf6105645
Laszlo Balazs-Csiki <lbalazscs@users.noreply.github.com>
2021-05-26 12:02:13 +0200

    small improvements

--- src/main/java/pixelitor/filters/Spiral.java
+++ src/main/java/pixelitor/filters/Spiral.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Laszlo Balazs-Csiki and Contributors
+ * Copyright 2021 Laszlo Balazs-Csiki and Contributors
  *
  * This file is part of Pixelitor. Pixelitor is free software: you
  * can redistribute it and/or modify it under the terms of the GNU
@@ -23,8 +23,6 @@ import pixelitor.filters.gui.FilterSetting;
 import pixelitor.filters.gui.IntChoiceParam;
 import pixelitor.filters.gui.RangeParam;
 
-import javax.swing.event.ListDataEvent;
-import javax.swing.event.ListDataListener;
 import java.awt.geom.Path2D;
 
 /**
@@ -34,12 +32,14 @@ public class Spiral extends ShapeFilter {
     public static final String NAME = "Spiral";
 
     private static final int NUM_STEPS_PER_SPIN = 100;
+    private static final int TYPE_CIRCULAR = 0;
+    private static final int TYPE_POLYGON = 1;
 
     private final RangeParam numSpinsParam = new RangeParam("Number of Spins",
-            1, 3, 10);
+        1, 3, 10);
     private final IntChoiceParam typeParam = new IntChoiceParam("Type", new IntChoiceParam.Item[]{
-            new IntChoiceParam.Item("Circular", 0),
-            new IntChoiceParam.Item("Polygon", 1)
+        new IntChoiceParam.Item("Circular", TYPE_CIRCULAR),
+        new IntChoiceParam.Item("Polygon", TYPE_POLYGON)
     });
     private final RangeParam sidesParam = new RangeParam("Sides", 3, 4, 10);
     private final BooleanParam symmetry = new BooleanParam("Symmetric", false);
@@ -49,27 +49,21 @@ public class Spiral extends ShapeFilter {
         sidesParam.setEnabled(false, FilterSetting.EnabledReason.APP_LOGIC);
 
         addParamsToFront(
-                numSpinsParam,
-                typeParam,
-                sidesParam,
-                symmetry,
-                scale
+            numSpinsParam,
+            typeParam,
+            sidesParam,
+            symmetry,
+            scale
         );
 
-        typeParam.addListDataListener(new TypeParamListener());
+        typeParam.setupEnableOtherIf(sidesParam, type ->
+            type.getValue() == TYPE_POLYGON);
 
         helpURL = "https://en.wikipedia.org/wiki/Spiral";
     }
 
     @Override
     protected Path2D createShape(int width, int height) {
-        return switch (typeParam.getValue()) {
-            default -> createCircular(width, height);
-            case 1 -> createPolygonal(width, height);
-        };
-    }
-
-    private Path2D createCircular(int width, int height) {
         Path2D shape = new Path2D.Double();
 
         double cx = width * center.getRelativeX();
@@ -79,42 +73,18 @@ public class Spiral extends ShapeFilter {
 
         double w = width / 2.0;
         double h = height / 2.0;
-        double maxAngle = 2 * Math.PI * numSpins;
-        double dt = maxAngle / (NUM_STEPS_PER_SPIN * numSpins);
-        double a = 1.0 / maxAngle;
-
-        shape.moveTo(cx, cy);
-        for (double t = dt; t < maxAngle; t += dt) {
-            double x = w * a * t * FastMath.cos(t) + cx;
-            double y = h * a * t * FastMath.sin(t) + cy;
-            shape.lineTo(x, y);
-        }
-        if (symmetry.isChecked()) {
-            shape.moveTo(cx, cy);
-            for (double t = dt; t < maxAngle; t += dt) {
-                double x = -w * a * t * FastMath.cos(t) + cx;
-                double y = -h * a * t * FastMath.sin(t) + cy;
-                shape.lineTo(x, y);
-            }
-        }
-
-        return shape;
-    }
-
-    private Path2D createPolygonal(int width, int height) {
-        Path2D shape = new Path2D.Double();
-
-        double cx = width * center.getRelativeX();
-        double cy = height * center.getRelativeY();
-
-        double w = width / 2.0;
-        double h = height / 2.0;
-
-        if (!scale.isChecked())
+        if (!scale.isChecked()) {
             w = h = Math.max(w, h);
+        }
 
-        double maxAngle = 2 * Math.PI * numSpinsParam.getValue();
-        double dt = (2 * Math.PI) / sidesParam.getValue();
+        double maxAngle = 2 * Math.PI * numSpins;
+
+        double dt = switch (typeParam.getValue()) {
+            case TYPE_CIRCULAR -> maxAngle / (NUM_STEPS_PER_SPIN * numSpins);
+            case TYPE_POLYGON -> (2 * Math.PI) / sidesParam.getValue();
+            default -> throw new IllegalStateException("Unexpected value: " + typeParam.getValue());
+        };
+
         double a = 1.0 / maxAngle;
 
         shape.moveTo(cx, cy);
@@ -133,44 +103,10 @@ public class Spiral extends ShapeFilter {
         }
 
         return shape;
-    }
-
-    private Path2D createRectangular(int width, int height) {
-        return null;
     }
 
     @Override
     protected float getGradientRadius(float cx, float cy) {
         return Math.min(cx, cy);
-    }
-
-
-    //
-
-
-    //
-
-
-    class TypeParamListener implements ListDataListener {
-        @Override
-        public void intervalAdded(ListDataEvent e) {
-        }
-
-        @Override
-        public void intervalRemoved(ListDataEvent e) {
-        }
-
-        @Override
-        public void contentsChanged(ListDataEvent e) {
-
-            // If it's circular, we don't need a parameter for sides
-            if (typeParam.getValue() == 0)
-                sidesParam.setEnabled(false, FilterSetting.EnabledReason.APP_LOGIC);
-
-                // If it's polygonal, we need need a parameter to know it's sides
-            else
-                sidesParam.setEnabled(true, FilterSetting.EnabledReason.APP_LOGIC);
-
-        }
     }
 }
