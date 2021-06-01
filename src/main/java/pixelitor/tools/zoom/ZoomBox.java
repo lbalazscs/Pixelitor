7d75dc262ef2ab6f63aa530890ee46c32eed8a59
MCXIV <mcxiv14@gmail.com>
2021-06-01 10:55:02 +0530

    Updated

--- src/main/java/pixelitor/tools/zoom/ZoomBox.java
+++ src/main/java/pixelitor/tools/zoom/ZoomBox.java
@@ -5,29 +5,16 @@ import pixelitor.gui.View;
 import pixelitor.tools.ToolWidget;
 import pixelitor.tools.crop.CropHandle;
 import pixelitor.tools.util.ArrowKey;
-import pixelitor.tools.util.PMouseEvent;
 import pixelitor.tools.util.PRectangle;
-import pixelitor.utils.Cursors;
-import pixelitor.utils.Shapes;
 
 import java.awt.*;
-import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 
 public class ZoomBox implements ToolWidget {
 
     private static final int MODE_NONE = 0;
-    private static final int MODE_MOVE = 1;
-    private static final int MODE_RESIZE = 2;
     private final PRectangle rect;
-    private int upperLeftX = 0;
-    private int upperLeftY = 0;
-    private int lowerRightX = 0;
-    private int lowerRightY = 0;
-    private int dragStartCursorType;
-    private Rectangle dragStartRect;
-    private Point dragStart;
 
     // type of user transform
     private int transformMode = MODE_NONE;
@@ -37,237 +24,15 @@ public class ZoomBox implements ToolWidget {
 
     public ZoomBox(PRectangle rect, View view) {
         this.rect = rect;
-        updateHandles();
-    }
-
-    public static double calcAspectRatio(Rectangle rect) {
-        if (rect.height > 0) {
-            return (double) rect.width / rect.height;
-        } else {
-            return 0;
-        }
-    }
-
-    private static boolean isResizeMode(int cursorType) {
-        switch (cursorType) {
-            case Cursor.NW_RESIZE_CURSOR:
-            case Cursor.SE_RESIZE_CURSOR:
-            case Cursor.SW_RESIZE_CURSOR:
-            case Cursor.NE_RESIZE_CURSOR:
-            case Cursor.N_RESIZE_CURSOR:
-            case Cursor.S_RESIZE_CURSOR:
-            case Cursor.E_RESIZE_CURSOR:
-            case Cursor.W_RESIZE_CURSOR:
-                return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Recalculates the box position and size, according to the mouse offset and direction
-     *
-     * @param rect       rectangle to adjust
-     * @param cursorType user modification direction (N,S,W,E,NW,NE,SE,SW)
-     * @param moveOffset the offset calculated as: mousePos - mouseStartPos
-     */
-    public static void resize(Rectangle rect, int cursorType, Point moveOffset) {
-        int offsetX = moveOffset.x;
-        int offsetY = moveOffset.y;
-        switch (cursorType) {
-            case Cursor.NW_RESIZE_CURSOR -> {
-                rect.width -= offsetX;
-                rect.height -= offsetY;
-                rect.x += offsetX;
-                rect.y += offsetY;
-            }
-            case Cursor.SE_RESIZE_CURSOR -> {
-                rect.width += offsetX;
-                rect.height += offsetY;
-            }
-            case Cursor.SW_RESIZE_CURSOR -> {
-                rect.width -= offsetX;
-                rect.height += offsetY;
-                rect.x += offsetX;
-            }
-            case Cursor.NE_RESIZE_CURSOR -> {
-                rect.width += offsetX;
-                rect.height -= offsetY;
-                rect.y += offsetY;
-            }
-            case Cursor.N_RESIZE_CURSOR -> {
-                rect.height -= offsetY;
-                rect.y += offsetY;
-            }
-            case Cursor.S_RESIZE_CURSOR -> rect.height += offsetY;
-            case Cursor.E_RESIZE_CURSOR -> rect.width += offsetX;
-            case Cursor.W_RESIZE_CURSOR -> {
-                rect.width -= offsetX;
-                rect.x += offsetX;
-            }
-        }
-    }
-
-    /**
-     * Adjusts the given rectangle to keep the aspect ratio.
-     * Recalculates the x,y according to the drag direction.
-     *
-     * @param rect        rectangle to adjust
-     * @param cursorType  user modification direction (N,S,W,E,NW,NE,SE,SW)
-     * @param aspectRatio aspect ratio of original rectangle. Required > 0
-     */
-    public static void keepAspectRatio(Rectangle rect, int cursorType, double aspectRatio) {
-        switch (cursorType) {
-            case Cursor.NW_RESIZE_CURSOR:
-            case Cursor.NE_RESIZE_CURSOR:
-            case Cursor.SE_RESIZE_CURSOR:
-            case Cursor.SW_RESIZE_CURSOR:
-                // To find out whether to adjust the width
-                // or the height, compare the rectangle ratios.
-                double aspectRatioNew = calcAspectRatio(rect);
-                if (aspectRatioNew > aspectRatio) {
-                    int height = (int) (rect.width / aspectRatio);
-                    if (cursorType == Cursor.NW_RESIZE_CURSOR ||
-                            cursorType == Cursor.NE_RESIZE_CURSOR) {
-                        rect.y -= (height - rect.height);
-                    }
-
-                    rect.height = height;
-                } else {
-                    int width = (int) (rect.height * aspectRatio);
-                    if (cursorType == Cursor.NW_RESIZE_CURSOR ||
-                            cursorType == Cursor.SW_RESIZE_CURSOR) {
-                        rect.x -= (width - rect.width);
-                    }
-
-                    rect.width = width;
-                }
-                break;
-            case Cursor.N_RESIZE_CURSOR:
-            case Cursor.S_RESIZE_CURSOR:
-                // adjust width and center horizontally
-                int width = (int) (rect.height * aspectRatio);
-                rect.x -= (width - rect.width) / 2;
-                rect.width = width;
-                break;
-            case Cursor.E_RESIZE_CURSOR:
-            case Cursor.W_RESIZE_CURSOR:
-                // adjust height and center vertically
-                int height = (int) (rect.width / aspectRatio);
-                rect.y -= (height - rect.height) / 2;
-                rect.height = height;
-                break;
-        }
     }
 
     @Override
     public void paint(Graphics2D g) {
-        drawRect(g);
-    }
-
-    private void drawRect(Graphics2D g) {
-        Shapes.drawVisibly(g, getSelectedCoRect());
-    }
-
-    private Rectangle getSelectedCoRect() {
-        return Shapes.toPositiveRect(upperLeftX, lowerRightX, upperLeftY, lowerRightY);
-    }
-
-    /**
-     * Iterates over all the handles and if it finds one that is
-     * under the given mouse coordinates, sets the cursor accordingly.
-     *
-     * @return true if cursor was set on any handle, false otherwise
-     */
-    private boolean setCursorForPoint(double x, double y, View view) {
-        CropHandle hit = handleWasHit(x, y);
-        if (hit != null) {
-            view.setCursor(hit.getCursor());
-            return true;
-        }
-
-        return false;
     }
 
     @Override
     public CropHandle handleWasHit(double x, double y) {
         return null;
-    }
-
-    private void updateHandles() {
-        Rectangle r = rect.getCo();
-
-        upperLeftX = r.x;
-        upperLeftY = r.y;
-
-        lowerRightX = r.x + r.width;
-        lowerRightY = r.y + r.height;
-    }
-
-    public void mousePressed(PMouseEvent e) {
-        View view = e.getView();
-        dragStart = e.getPoint();
-        dragStartRect = new Rectangle(rect.getCo());
-        dragStartCursorType = view.getCursor().getType();
-        aspectRatio = calcAspectRatio(rect.getCo());
-
-        if (isResizeMode(dragStartCursorType)) {
-            // if user clicked on the handle allow resize it
-            transformMode = MODE_RESIZE;
-        } else if (rect.containsCo(e.getPoint())) {
-            // if user clicked inside selection allow relocate it
-            transformMode = MODE_MOVE;
-        } else {
-            transformMode = MODE_NONE;
-        }
-    }
-
-    public void mouseDragged(PMouseEvent e) {
-        if (transformMode == MODE_NONE) {
-            return;
-        }
-
-        rect.getCo().setRect(dragStartRect);
-        Point mouseOffset = new Point(
-                (int) (e.getCoX() - dragStart.x),
-                (int) (e.getCoY() - dragStart.y));
-
-        if (transformMode == MODE_RESIZE) {
-            resize(rect.getCo(), dragStartCursorType, mouseOffset);
-
-            if (e.isShiftDown() && aspectRatio > 0) {
-                keepAspectRatio(rect.getCo(), dragStartCursorType, aspectRatio);
-            }
-        } else if (transformMode == MODE_MOVE) {
-            rect.getCo().translate(mouseOffset.x, mouseOffset.y);
-        }
-
-        updateHandles();
-
-        rect.makeCoPositive();
-        rect.recalcIm(e.getView());
-    }
-
-    public void mouseReleased(PMouseEvent e) {
-        // ensure that after resize rectangle has positive
-        // width and height (required for Rectangle.contain testing)
-        rect.makeCoPositive();
-
-        updateHandles();
-        setCursorForPoint(e.getCoX(), e.getCoY(), e.getView());
-
-        transformMode = MODE_NONE;
-    }
-
-    public void mouseMoved(MouseEvent e, View view) {
-        boolean isCursorSet = setCursorForPoint(e.getX(), e.getY(), view);
-        if (!isCursorSet) {
-            if (rect.containsCo(e.getX(), e.getY())) {
-                view.setCursor(Cursors.MOVE);
-            } else {
-                view.setCursor(Cursors.DEFAULT);
-            }
-        }
     }
 
     public PRectangle getRect() {
@@ -277,26 +42,11 @@ public class ZoomBox implements ToolWidget {
     @Override
     public void coCoordsChanged(View view) {
         rect.coCoordsChanged(view);
-        updateHandles();
     }
 
     @Override
     public void imCoordsChanged(AffineTransform at, Composition comp) {
         rect.imCoordsChanged(comp.getView(), at);
-        updateHandles();
-    }
-
-    /**
-     * Sets the size of the crop box in image space.
-     */
-    public void setImSize(int width, int height, View view) {
-        Rectangle2D imRect = rect.getIm();
-        imRect.setRect(imRect.getX(), imRect.getY(), width, height);
-
-        rect.recalcCo(view);
-
-        updateHandles();
-        view.repaint();
     }
 
     /**
@@ -308,30 +58,11 @@ public class ZoomBox implements ToolWidget {
 
     @Override
     public void arrowKeyPressed(ArrowKey key, View view) {
-        // Two situations have to be taken into consideration:
-        // 1. if the zoom level is >= 100%, then move the rectangle by 1px
-        // 2. if the zoom level is < 100%, then scale up to ensure
-        //    that the user always sees moving the rectangle
-        double viewScale = view.getZoomLevel().getViewScale();
-        int moveScale = viewScale >= 1 ? 1 : (int) Math.ceil(1 / viewScale);
-
-        Rectangle2D im = rect.getIm();
-        im.setRect(
-                im.getX() + key.getMoveX() * moveScale,
-                im.getY() + key.getMoveY() * moveScale,
-                im.getWidth(),
-                im.getHeight()
-        );
-
-        rect.recalcCo(view);
-        updateHandles();
-        view.repaint();
     }
 
     @Override
     public String toString() {
-        return "ZoomBox{ upperLeft=" + upperLeftX + ", " + upperLeftY +
-                ", lowerRight=" + lowerRightX + ", " + lowerRightY +
+        return "ZoomBox{ " +
                 ", rect=" + rect +
                 ", adjusting=" + isAdjusting() +
                 ", transformMode=" + transformMode +
--- src/main/java/pixelitor/tools/zoom/ZoomTool.java
+++ src/main/java/pixelitor/tools/zoom/ZoomTool.java
@@ -4,45 +4,30 @@ import pixelitor.AppContext;
 import pixelitor.Canvas;
 import pixelitor.Composition;
 import pixelitor.OpenImages;
-import pixelitor.filters.gui.RangeParam;
 import pixelitor.gui.AutoZoom;
 import pixelitor.gui.View;
-import pixelitor.gui.utils.GUIUtils;
-import pixelitor.gui.utils.SliderSpinner;
-import pixelitor.guides.GuidesRenderer;
 import pixelitor.menus.view.ZoomLevel;
 import pixelitor.tools.DragTool;
 import pixelitor.tools.DragToolState;
 import pixelitor.tools.Tool;
-import pixelitor.tools.crop.CompositionGuide;
-import pixelitor.tools.crop.CompositionGuideType;
-import pixelitor.tools.util.ArrowKey;
 import pixelitor.tools.util.PMouseEvent;
 import pixelitor.tools.util.PRectangle;
 import pixelitor.utils.Cursors;
-import pixelitor.utils.Messages;
+import pixelitor.utils.Shapes;
 import pixelitor.utils.debug.DebugNode;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
 import java.awt.geom.*;
 
-import static java.awt.AlphaComposite.SRC_OVER;
 import static java.awt.BasicStroke.CAP_BUTT;
 import static java.awt.BasicStroke.JOIN_MITER;
-import static java.awt.Color.BLACK;
-import static pixelitor.gui.utils.SliderSpinner.TextPosition.WEST;
 import static pixelitor.tools.DragToolState.*;
 
 public class ZoomTool extends DragTool {
 
-    private final RangeParam maskOpacity = new RangeParam("Mask Opacity (%)", 0, 75, 100);
     private DragToolState state = NO_INTERACTION;
     private ZoomBox zoomBox;
-    private Composite maskComposite = AlphaComposite.getInstance(SRC_OVER, maskOpacity.getPercentageValF());
-    private JComboBox<CompositionGuideType> guidesCB;
 
     public ZoomTool() { // Do I need this false in super call?
         super("Zoom", 'Z', "zoom_tool.png",
@@ -55,68 +40,26 @@ public class ZoomTool extends DragTool {
 
     @Override
     public void initSettingsPanel() {
-        addMaskOpacitySelector();
-
-        settingsPanel.addSeparator();
-        addGuidesSelector();
-
         if (AppContext.isDevelopment()) {
-            JButton b = new JButton("Dump State");
-            b.addActionListener(e -> {
-                View view = OpenImages.getActiveView();
-                Canvas canvas = view.getCanvas();
-                System.out.println("ZoomTool::actionPerformed: canvas = " + canvas);
-                System.out.println("ZoomTool::initSettingsPanel: state = " + state);
-                System.out.println("ZoomTool::initSettingsPanel: dumpBox = " + zoomBox);
-            });
-            settingsPanel.add(b);
+            settingsPanel.add(new JButton("Dump State") {{
+                addActionListener(e -> {
+                    System.out.println("ZoomTool::actionPerformed: canvas = " + OpenImages.getActiveView().getCanvas());
+                    System.out.println("ZoomTool::initSettingsPanel: state = " + state);
+                    System.out.println("ZoomTool::initSettingsPanel: dumpBox = " + zoomBox);
+                });
+            }});
         }
-    }
-
-    private void addMaskOpacitySelector() {
-        maskOpacity.addChangeListener(e -> maskOpacityChanged());
-        SliderSpinner maskOpacitySpinner = new SliderSpinner(
-                maskOpacity, WEST, false);
-        settingsPanel.add(maskOpacitySpinner);
-    }
-
-    private void maskOpacityChanged() {
-        float alpha = maskOpacity.getPercentageValF();
-        // because of a swing bug, the slider can get out of range?
-        if (alpha < 0.0f) {
-            System.out.printf("ZoomTool::maskOpacityChanged: alpha = %.2f%n", alpha);
-            alpha = 0.0f;
-            maskOpacity.setValue(0);
-        } else if (alpha > 1.0f) {
-            System.out.printf("ZoomTool::maskOpacityChanged: alpha = %.2f%n", alpha);
-            alpha = 1.0f;
-            maskOpacity.setValue(100);
-        }
-        maskComposite = AlphaComposite.getInstance(SRC_OVER, alpha);
-        OpenImages.repaintActive();
-    }
-
-    private void addGuidesSelector() {
-        guidesCB = GUIUtils.createComboBox(CompositionGuideType.values());
-        guidesCB.setToolTipText("<html>Composition guides." +
-                "<br><br>Press <b>O</b> to select the next guide." +
-                "<br>Press <b>Shift-O</b> to change the orientation.");
-        guidesCB.addActionListener(e -> OpenImages.repaintActive());
-        settingsPanel.addComboBox("Guides:", guidesCB, "guidesCB");
     }
 
     @Override
     public void mouseClicked(PMouseEvent e) {
         Point mousePos = e.getPoint();
         View view = e.getView();
-        if (e.isLeft()) {
-            if (e.isAltDown()) {
-                view.decreaseZoom(mousePos);
-            } else {
-                view.increaseZoom(mousePos);
-            }
-        } else if (e.isRight()) {
+
+        if (e.isRight() || (e.isLeft() && e.isAltDown())) {
             view.decreaseZoom(mousePos);
+        } else {
+            view.increaseZoom(mousePos);
         }
     }
 
@@ -127,65 +70,30 @@ public class ZoomTool extends DragTool {
         } else if (state == INITIAL_DRAG) {
             throw new IllegalStateException();
         }
-
-        if (state == TRANSFORM) {
-            assert zoomBox != null;
-            zoomBox.mousePressed(e);
-        }
     }
 
     @Override
     public void ongoingDrag(PMouseEvent e) {
-        if (state == TRANSFORM) {
-            zoomBox.mouseDragged(e);
-        } else if (userDrag != null) {
-            userDrag.setStartFromCenter(e.isAltDown());
-            userDrag.setEquallySized(e.isShiftDown());
-        }
-
-        // in the USER_DRAG state this will also
-        // cause the painting of the darkening overlay
         e.repaint();
     }
 
     @Override
-    public void mouseMoved(MouseEvent e, View view) {
-        super.mouseMoved(e, view);
-        if (state == TRANSFORM) {
-            zoomBox.mouseMoved(e, view);
-        }
-    }
-
-    @Override
     public void dragFinished(PMouseEvent e) {
-        var comp = e.getComp();
-        comp.update();
+        if (state == NO_INTERACTION) {
+            return;
+        }
 
-        switch (state) {
-            case NO_INTERACTION:
-                break;
-            case INITIAL_DRAG:
-                if (zoomBox != null) {
-                    throw new IllegalStateException();
-                }
+        if (state == INITIAL_DRAG) {
+            if (zoomBox != null) {
+                throw new IllegalStateException();
+            }
 
-                Rectangle r = userDrag.toCoRect();
-                PRectangle rect = PRectangle.positiveFromCo(r, e.getView());
+            View view = e.getView();
+            zoomBox = new ZoomBox(PRectangle.positiveFromCo(userDrag.toCoRect(), view), view);
+            setState(TRANSFORM);
 
-                zoomBox = new ZoomBox(rect, e.getView());
-
-                setState(TRANSFORM);
-
-                if (executeZoomCommand(e.getView())) {
-                    e.consume();
-                }
-                break;
-            case TRANSFORM:
-                if (zoomBox == null) {
-                    throw new IllegalStateException();
-                }
-                zoomBox.mouseReleased(e);
-                break;
+            executeZoomCommand(view);
+            e.consume();
         }
     }
 
@@ -199,46 +107,7 @@ public class ZoomTool extends DragTool {
             return;
         }
 
-        paintDarkMask(g2, comp, zoomRect);
-
-        if (state == TRANSFORM) {
-            paintBox(g2, zoomRect);
-        }
-    }
-
-    // Paint the semi-transparent dark area outside the zoom rectangle.
-    // All calculations are in component space.
-    private void paintDarkMask(Graphics2D g2, Composition comp, PRectangle zoomRect) {
-
-        // The Swing clip ensures that we can't draw outside the component,
-        // even if the canvas is outside of it (scrollbars)
-        Shape origClip = g2.getClip();
-        Color origColor = g2.getColor();
-        Composite origComposite = g2.getComposite();
-
-        View view = comp.getView();
-        Rectangle2D coCanvasBounds = comp.getCanvas().getCoBounds(view);
-
-        Rectangle2D visibleCanvasArea = coCanvasBounds.createIntersection(
-                view.getVisiblePart());
-
-        Path2D maskAreaClip = new Path2D.Double(Path2D.WIND_EVEN_ODD);
-        maskAreaClip.append(visibleCanvasArea, false);
-        maskAreaClip.append(zoomRect.getCo(), false); // subtract the zoom rect
-
-        g2.setColor(BLACK);
-        g2.setComposite(maskComposite);
-        g2.setClip(maskAreaClip);
-        g2.fill(visibleCanvasArea);
-
-        g2.setColor(origColor);
-        g2.setComposite(origComposite);
-        g2.setClip(origClip);
-    }
-
-    // Paint the handles and the guides.
-    private void paintBox(Graphics2D g2, PRectangle zoomRect) {
-        zoomBox.paint(g2);
+        Shapes.drawVisibly(g2, zoomRect.getCo());
     }
 
     /**
@@ -266,7 +135,7 @@ public class ZoomTool extends DragTool {
         setState(NO_INTERACTION);
 
         OpenImages.repaintActive();
-        OpenImages.setCursorForAll(Cursors.DEFAULT);
+        OpenImages.setCursorForAll(Cursors.HAND);
     }
 
     private void setState(DragToolState newState) {
@@ -294,110 +163,45 @@ public class ZoomTool extends DragTool {
         }
     }
 
-    @Override
-    public boolean arrowKeyPressed(ArrowKey key) {
-        if (state == TRANSFORM) {
-            View view = OpenImages.getActiveView();
-            if (view != null) {
-                zoomBox.arrowKeyPressed(key, view);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public void escPressed() {
-        executeCancelCommand();
-    }
-
-    // return true if a zoom was executed
-    private boolean executeZoomCommand(View view) {
+    private void executeZoomCommand(View view) {
 
         Rectangle2D zoomRect = getZoomRect().getIm();
         if (zoomRect.isEmpty()) {
-            return false;
-        }
-
-//        Rectangle bounds = OpenImages.getActiveComp().getCanvas().getBounds();
-//bounds = view.imageToComponentSpace(bounds);
-
-        Canvas c = new Canvas((int) zoomRect.getWidth(), (int) zoomRect.getHeight());
-
-//        ZoomLevel.Z18;
-//        view.setZoom(ZoomLevel.valueOf());
-        view.setZoom(ZoomLevel.calcZoom(c, AutoZoom.FIT_SPACE, true));
-        view.scrollRectToVisible(getZoomRect().getCo());
-
-        resetInitialState();
-        return true;
-    }
-
-    private void executeCancelCommand() {
-        if (state != TRANSFORM) {
             return;
         }
 
+        Canvas c = new Canvas((int) zoomRect.getWidth(), (int) zoomRect.getHeight());
+
+        view.setZoom(ZoomLevel.calcZoom(c, AutoZoom.FIT_SPACE, true));
+
+        view.scrollRectToVisible(getZoomRect().getCo());
+
         resetInitialState();
-        Messages.showPlainInStatusBar("Zoom canceled.");
-    }
-
-    @Override
-    public void otherKeyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-//            if (executeZoomCommand()) { // TODO
-//                e.consume();
-//            }
-            // otherwise the Enter might be used elsewhere,
-            // for example by the layer name editor
-        } else if (e.getKeyCode() == KeyEvent.VK_O) {
-            if (e.isControlDown()) {
-                // ignore Ctrl-O see issue #81
-                return;
-            }
-            if (e.isShiftDown()) {
-                // Shift-O: change the orientation
-                // within the current composition guide family
-                if (state == TRANSFORM) {
-                    OpenImages.repaintActive();
-                    e.consume();
-                }
-            } else {
-                // O: advance to the next composition guide
-                selectTheNextCompositionGuide();
-                e.consume();
-            }
-        }
-    }
-
-    private void selectTheNextCompositionGuide() {
-        int index = guidesCB.getSelectedIndex();
-        int itemCount = guidesCB.getItemCount();
-        int nextIndex;
-        if (index == itemCount - 1) {
-            nextIndex = 0;
-        } else {
-            nextIndex = index + 1;
-        }
-        guidesCB.setSelectedIndex(nextIndex);
     }
 
     @Override
     public DebugNode createDebugNode() {
         var node = super.createDebugNode();
-
-        node.addFloat("mask opacity", maskOpacity.getPercentageValF());
         node.addString("state", state.toString());
-
         return node;
     }
 
     @Override
     public Icon createIcon() {
-        return new ZoomToolIcon();
+        return ZoomToolIcon.POSITIVE;
     }
 
     private static class ZoomToolIcon extends Tool.ToolIcon {
+
+        public static final ToolIcon POSITIVE = new ZoomToolIcon(true);
+        public static final ToolIcon NEGETIVE = new ZoomToolIcon(false);
+
+        private final boolean drawMinus;
+
+        public ZoomToolIcon(boolean drawMinus) {
+            this.drawMinus = drawMinus;
+        }
+
         @Override
         public void paintIcon(Graphics2D g) {
             // based on zoom_tool.svg
@@ -409,8 +213,10 @@ public class ZoomTool extends DragTool {
             Line2D hor = new Line2D.Double(12.0, 10.0, 24.0, 10.0);
             g.draw(hor);
 
-            Line2D ver = new Line2D.Double(18.0, 16.0, 18.0, 4.0);
-            g.draw(ver);
+            if (drawMinus) {
+                Line2D ver = new Line2D.Double(18.0, 16.0, 18.0, 4.0);
+                g.draw(ver);
+            }
 
             Path2D shape = new Path2D.Float();
             shape.moveTo(13.447782, 17.801485);
