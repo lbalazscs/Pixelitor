1b98bfe7d29729d923ec12545c04241e9c897ec6
Laszlo Balazs-Csiki <lbalazscs@users.noreply.github.com>
2021-06-02 19:20:54 +0200

    change next version number to 4.3.0

--- src/main/java/pixelitor/Pixelitor.java
+++ src/main/java/pixelitor/Pixelitor.java
@@ -50,7 +50,7 @@ import static pixelitor.utils.Threads.*;
  * The main class
  */
 public class Pixelitor {
-    public static final String VERSION_NUMBER = "4.2.4";
+    public static final String VERSION_NUMBER = "4.3.0";
     public static Locale SYS_LOCALE;
 
     private Pixelitor() {
