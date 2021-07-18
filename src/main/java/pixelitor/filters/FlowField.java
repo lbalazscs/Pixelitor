664946a250a3125a713839ef7c21a5540c518eb7
MCXIV <mcxiv14@gmail.com>
2021-07-18 21:46:52 +0530

    Boom

--- src/main/java/pixelitor/filters/FlowField.java
+++ src/main/java/pixelitor/filters/FlowField.java
@@ -79,7 +79,7 @@ public class FlowField extends ParametrizedFilter {
 
         Graphics2D g2 = dest.createGraphics();
 //        g2.setStroke(stroke);
-        g2.setStroke(new VanishingStroke());
+        g2.setStroke(new VanishingStroke(20));
 
         int field_w = (int) (w * field_density) + 1;
         int field_h = (int) (h * field_density) + 1;
