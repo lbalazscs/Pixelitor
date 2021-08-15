df26c72ac0bf1fad91a844089c8a2a30a8bd2585
MCXIV <mcxiv14@gmail.com>
2021-08-15 15:49:40 +0530

    Added antiliasing option

--- src/main/java/pixelitor/filters/FlowField.java
+++ src/main/java/pixelitor/filters/FlowField.java
@@ -136,13 +136,17 @@ public class FlowField extends ParametrizedFilter {
     private final RangeParam sinkParam = new RangeParam("Sink", 0, 0, 100);
     private final RangeParam revolveParam = new RangeParam("Revolve", 0, 0, 100);
 
-    private final RangeParam particlesParam = new RangeParam("Particle Count", 1, 1000, 20000, true, BORDER, IGNORE_RANDOMIZE);
+    private final RangeParam zoomParam = new RangeParam("Zoom (%)", 100, 4000, 10000);
+    private final RangeParam varianceParam = new RangeParam("Variance", 1, 20, 100);
+    private final RangeParam turbulenceParam = new RangeParam("Turbulence", 1, 1, 8);
+    private final RangeParam windParam = new RangeParam("Wind", 0, 0, 200);
 
     private final EnumParam<ForceMode> forceModeParam = new EnumParam<>("Force Mode", ForceMode.class);
     private final RangeParam maxVelocityParam = new RangeParam("Maximum Velocity", 1, 4000, 5000);
-    private final RangeParam varianceParam = new RangeParam("Variance", 1, 20, 100);
+    private final RangeParam iterationsParam = new RangeParam("Path Length (Makes simulation slow!!)", 1, 100, 5000, true, BORDER, IGNORE_RANDOMIZE);
+    private final BooleanParam antialiasParam = new BooleanParam("Use Antialiasing", false);
 
-    private final RangeParam zoomParam = new RangeParam("Zoom (%)", 100, 4000, 10000);
+    private final RangeParam particlesParam = new RangeParam("Particle Count", 1, 1000, 20000, true, BORDER, IGNORE_RANDOMIZE);
     private final StrokeParam strokeParam = new StrokeParam("Stroke");
     private final ColorParam backgroundColorParam = new ColorParam("Background Color", new Color(0, 0, 0, 1.0f), FREE_TRANSPARENCY);
     private final ColorParam particleColorParam = new ColorParam("Particle Color", new Color(1, 1, 1, 0.12f), FREE_TRANSPARENCY);
@@ -155,10 +159,6 @@ public class FlowField extends ParametrizedFilter {
     private final BooleanParam useSourceImageAsStartingPositionParam = new BooleanParam("Start flow from source,", false, IGNORE_RANDOMIZE);
     private final RangeParam colorRandomnessParam = new RangeParam("Color Randomness (%)", 0, 0, 100);
     private final RangeParam radiusRandomnessParam = new RangeParam("Stroke Width Randomness (%)", 0, 0, 1000);
-
-    private final RangeParam iterationsParam = new RangeParam("Path Length (Makes simulation slow!!)", 1, 100, 5000, true, BORDER, IGNORE_RANDOMIZE);
-    private final RangeParam turbulenceParam = new RangeParam("Turbulence", 1, 1, 8);
-    private final RangeParam windParam = new RangeParam("Wind", 0, 0, 200);
 
     //</editor-fold>
 
@@ -176,7 +176,7 @@ public class FlowField extends ParametrizedFilter {
         DialogParam noiseAdjustmentParam = new DialogParam("Noise", zoomParam, varianceParam, turbulenceParam, windParam);
         noiseParam.setupEnableOtherIfNotZero(noiseAdjustmentParam);
 
-        DialogParam advancedParam = new DialogParam("Advanced", forceModeParam, maxVelocityParam, iterationsParam);
+        DialogParam advancedParam = new DialogParam("Advanced", forceModeParam, maxVelocityParam, iterationsParam, antialiasParam);
 
         setParams(
 
@@ -196,19 +196,23 @@ public class FlowField extends ParametrizedFilter {
 
         ).withAction(ReseedSupport.createSimplexAction());
 
-        this.noiseParam.setToolTip("Add smooth randomness to the flow of particles.");
+        noiseParam.setToolTip("Add smooth randomness to the flow of particles.");
         sinkParam.setToolTip("Make particles flow towards the center point.");
         revolveParam.setToolTip("Make particles flow around the center point.");
 
-        particlesParam.setToolTip("Adjust the number of particles flowing in the field.");
-
         noiseAdjustmentParam.setToolTip("Some additional tweaks to manipulate the evaluation system.");
-        forceModeParam.setToolTip("Advanced control over how forces act.");
-        maxVelocityParam.setToolTip("Adjust maximum velocity to make particles look more organised.");
-        varianceParam.setToolTip("Increase variance to add more turns and twists to the flow.");
-
         zoomParam
             .setToolTip("Change zoom to make fields look bigger or smaller. Please note, that particle width will not take any effect.");
+        varianceParam.setToolTip("Increase variance to add more turns and twists to the flow.");
+        turbulenceParam.setToolTip("Increase to make the flow path rougher.");
+        windParam.setToolTip("Spreads away the particles against the flow path.");
+
+        advancedParam.setToolTip("Advanced or Rarely used parameters which make slight adjustments to the simulation.");
+        forceModeParam.setToolTip("Advanced control over how forces act.");
+        maxVelocityParam.setToolTip("Adjust maximum velocity to make particles look more organised.");
+        iterationsParam.setToolTip("Make individual particles cover longer paths.");
+
+        particlesParam.setToolTip("Adjust the number of particles flowing in the field.");
         strokeParam.setToolTip("Adjust how particles are drawn - their width, shape, joins...");
         backgroundColorParam
             .setToolTip("Fills the canvas with a color. Decrease transparency to show the previous image.");
@@ -219,11 +223,6 @@ public class FlowField extends ParametrizedFilter {
         useSourceImageAsStartingPositionParam.setToolTip("Prevent particles from spawning at any transparent regions.");
         colorRandomnessParam.setToolTip("Increase to impart the particle color with some randomness.");
         radiusRandomnessParam.setToolTip("Increase to draw particles with a randomised width.");
-
-        advancedParam.setToolTip("Advanced or Rarely used parameters which make slight adjustments to the simulation.");
-        iterationsParam.setToolTip("Make individual particles cover longer paths.");
-        turbulenceParam.setToolTip("Increase to make the flow path rougher.");
-        windParam.setToolTip("Spreads away the particles against the flow path.");
 
     }
 
@@ -236,12 +235,17 @@ public class FlowField extends ParametrizedFilter {
 
         //<editor-fold defaultstate="collapsed" desc="FINAL LOCAL VARIABLES DECLARATIONS">
 
-        final int particleCount = particlesParam.getValue();
+        final float zoom = zoomParam.getValue() * 0.25f;
+        final float variance = varianceParam.getValue() / 10.0f;
+        final int turbulence = turbulenceParam.getValue();
+        final float zFactor = windParam.getValueAsFloat() / 10000;
+
         final ForceMode forceMode = forceModeParam.getSelected();
         final float maximumVelocitySq = maxVelocityParam.getValue() * maxVelocityParam.getValue() / 10000.0f;
-        final float variance = varianceParam.getValue() / 10.0f;
+        final int iterationCount = iterationsParam.getValue() + 1;
+        final boolean antialias = antialiasParam.isChecked();
 
-        final float zoom = zoomParam.getValue() * 0.25f;
+        final int particleCount = particlesParam.getValue();
         final Stroke stroke = strokeParam.createStroke();
         final Color bgColor = backgroundColorParam.getColor();
         final Color particleColor = particleColorParam.getColor();
@@ -249,10 +253,6 @@ public class FlowField extends ParametrizedFilter {
         final boolean inheritSpawnPoints = useSourceImageAsStartingPositionParam.isChecked();
         final float colorRandomness = colorRandomnessParam.getPercentageValF();
         final float radiusRandomness = radiusRandomnessParam.getPercentageValF();
-
-        final int iterationCount = iterationsParam.getValue() + 1;
-        final int turbulence = turbulenceParam.getValue();
-        final float zFactor = windParam.getValueAsFloat() / 10000;
 
         final float quality = min(QUALITY, SMOOTHNESS / zoom);
         final int tolerance = min(TOLERANCE, ((int) (iterationCount * ITERATION_TO_TOLERANCE_RATIO)));
@@ -292,6 +292,9 @@ public class FlowField extends ParametrizedFilter {
         g2.setStroke(stroke);
         Colors.fillWith(bgColor, g2, imgWidth, imgHeight);
         g2.setColor(particleColor);
+        if (antialias) {
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        }
 
         final Color[][] fieldColors = getIf(useColorField, () -> new Color[fieldWidth][fieldHeight]);
         final Stroke[] strokes = getIf(randomizeRadius, () -> new Stroke[100]);
