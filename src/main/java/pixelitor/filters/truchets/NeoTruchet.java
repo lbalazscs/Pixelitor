6fff6b2d9e2e03fe822bcf015c5aaf5b54c80294
Anirudh Sharma <mcxiv14@gmail.com>
2024-03-12 12:29:13 +0530

    Added show tile boundary and swatch boundary feature

--- src/main/java/pixelitor/filters/truchets/NeoTruchet.java
+++ src/main/java/pixelitor/filters/truchets/NeoTruchet.java
@@ -7,6 +7,7 @@ import pixelitor.filters.gui.RandomizePolicy;
 import pixelitor.filters.gui.RangeParam;
 import pixelitor.utils.ImageUtils;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
@@ -26,7 +27,8 @@ public class NeoTruchet extends ParametrizedFilter {
     private final TruchetParam truchetParam = new TruchetParam("Truchet Param", RandomizePolicy.ALLOW_RANDOMIZE);
     private final ColorParam bgColor = new ColorParam("Background Color", WHITE, USER_ONLY_TRANSPARENCY);
     private final ColorParam fgColor = new ColorParam("Foreground Color", BLACK, USER_ONLY_TRANSPARENCY);
-    private final BooleanParam showBoundary = new BooleanParam("Show Tile Boundary", false, IGNORE_RANDOMIZE);
+    private final BooleanParam showTileBoundary = new BooleanParam("Show Tile Boundary", false, IGNORE_RANDOMIZE);
+    private final BooleanParam showSwatchBoundary = new BooleanParam("Show Swatch Boundary", false, IGNORE_RANDOMIZE);
 
     public NeoTruchet() {
         super(true);
@@ -37,7 +39,8 @@ public class NeoTruchet extends ParametrizedFilter {
             truchetParam,
             bgColor,
             fgColor,
-            showBoundary
+            showTileBoundary,
+            showSwatchBoundary
         );
     }
 
@@ -60,17 +63,46 @@ public class NeoTruchet extends ParametrizedFilter {
         g.setColor(fgColor.getColor());
         g.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
 
-        int numTilesHor = W / w + 1;
-        int numTilesVer = H / h + 1;
+        int numSwatchesHor = W / w + 1;
+        int numSwatchesVer = H / h + 1;
 
-        int widthCovered = W - numTilesHor * w;
-        int heightCovered = H - numTilesVer * h;
+        int widthCovered = (W - numSwatchesHor * w) / 2;
+        int heightCovered = (H - numSwatchesVer * h) / 2;
 
-        for (int hi = 0; hi < numTilesVer; hi++) {
-            for (int wi = 0; wi < numTilesHor; wi++) {
-                g.translate(widthCovered + wi * w, heightCovered + hi * h);
+        for (int hi = 0; hi < numSwatchesVer; hi++) {
+            for (int wi = 0; wi < numSwatchesHor; wi++) {
+                int x = widthCovered + wi * w, y = heightCovered + hi * h;
+                g.translate(x, y);
                 swatch.draw(g, tileSize, lineWidth);
-                g.translate(-(widthCovered + wi * w), -(heightCovered + hi * h));
+                g.translate(-x, -y);
+            }
+        }
+
+        if (showTileBoundary.isChecked()) {
+            g.setColor(Color.RED);
+            for (int hi = 0; hi < numSwatchesVer; hi++) {
+                int y = heightCovered + hi * h;
+                for (int i = 0; i < swatch.getHeight(1); i++) {
+                    g.drawLine(0, y + i * tileSize, dest.getWidth(), y + i * tileSize);
+                }
+            }
+            for (int wi = 0; wi < numSwatchesHor; wi++) {
+                int x = widthCovered + wi * w;
+                for (int i = 0; i < swatch.getWidth(1); i++) {
+                    g.drawLine(x + i * tileSize, 0, x + i * tileSize, dest.getHeight());
+                }
+            }
+        }
+
+        if (showSwatchBoundary.isChecked()) {
+            g.setColor(Color.GREEN);
+            for (int hi = 0; hi < numSwatchesVer; hi++) {
+                int y = heightCovered + hi * h;
+                g.drawLine(0, y, dest.getWidth(), y);
+            }
+            for (int wi = 0; wi < numSwatchesHor; wi++) {
+                int x = widthCovered + wi * w;
+                g.drawLine(x, 0, x, dest.getHeight());
             }
         }
 
